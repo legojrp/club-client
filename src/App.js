7d72c292aeab6e5c1114a8fa72bcb5ead636efd7
Arya Buddha <40647186+AryaBuddha@users.noreply.github.com>
2022-08-31 23:17:11 -0400

    get ready for gcp

--- src/App.js
+++ src/App.js
@@ -1,154 +1,136 @@
-import React , {useEffect, useState} from 'react';
-import {notification, Typography, Modal, Button} from 'antd'
+import { Button, Modal, notification, Typography } from "antd";
+import React, { useEffect, useState } from "react";
 
-import Settings from './views/Settings'
-import axios from 'axios'
-import Navbar from './util/Navbar'
-import Error404 from './util/404'
-import Club from './views/ClubPage/ClubPage'
-import ClubBrowse from './views/ClubBrowse/ClubBrowse'
-import ClubSettings from './views/ClubSettings/ClubSettings'
-import ClubCreate from './views/ClubCreate'
-import {BrowserRouter,Switch, Route, useParams} from 'react-router-dom'
+import axios from "axios";
+import { BrowserRouter, Route, Switch, useParams } from "react-router-dom";
+import Error404 from "./util/404";
+import Navbar from "./util/Navbar";
+import ClubBrowse from "./views/ClubBrowse/ClubBrowse";
+import ClubCreate from "./views/ClubCreate";
+import Club from "./views/ClubPage/ClubPage";
+import ClubSettings from "./views/ClubSettings/ClubSettings";
+import Settings from "./views/Settings";
 
-import AuthContext from './contexts/AuthContext'
-import ClubContext from './util/ClubContext'
-import UserClubContext from './contexts/UserClubContext'
+import AuthContext from "./contexts/AuthContext";
+import UserClubContext from "./contexts/UserClubContext";
+import ClubContext from "./util/ClubContext";
 
-import Login from './views/Login'
-import Signup from './views/Signup'
-import Verify from './views/Verify'
-import ForgotPassword from './views/ForgotPassword'
-import ResetPassword from './views/ResetPassword'
+import ForgotPassword from "./views/ForgotPassword";
+import Login from "./views/Login";
+import ResetPassword from "./views/ResetPassword";
+import Signup from "./views/Signup";
+import Verify from "./views/Verify";
 
+import EmailModal from "./util/EmailModal";
 
-import EmailModal from './util/EmailModal'
+const { Text, Link } = Typography;
 
-const {Text, Link} = Typography
-
-const notifContent =() => {
-  return(
-    <Text>     
-      Have a feature you want? Found a bug? Please provide any feedback to us here
-      <Link href="https://ant.design" target="_blank">Ant Design</Link>
+const notifContent = () => {
+  return (
+    <Text>
+      Have a feature you want? Found a bug? Please provide any feedback to us
+      here
+      <Link href="https://ant.design" target="_blank">
+        Ant Design
+      </Link>
     </Text>
-  )
-}
-
-
-
+  );
+};
 
 const App = () => {
-  const [auth, setAuth] = useState({isAuth: false})
-  const [clubContext, setClubContext] = useState(null)
-  const [shownNotif, setShownNotif] = useState(false)
-  const [userClubContext, setUserClubContext] = useState(null)
+  const [auth, setAuth] = useState({ isAuth: false });
+  const [clubContext, setClubContext] = useState(null);
+  const [shownNotif, setShownNotif] = useState(false);
+  const [userClubContext, setUserClubContext] = useState(null);
 
-
-
-  console.log(`${process.env.REACT_APP_CLUB_API}`)
-
-
+  console.log(`${process.env.REACT_APP_CLUB_API}`);
 
   useEffect(() => {
-    document.title = "HSE Clubs"
-    
+    document.title = "HSE Clubs";
 
-    if(!auth.user){
-      testToken()
+    if (!auth.user) {
+      testToken();
     }
-    if( (!auth.isAuth || auth.loading) && localStorage.getItem("token")){
-      console.log("test token")
-      testToken()
-    }
-
-    if ((auth.isAuth && !auth.loading) && (!auth.name || !auth.phone || !auth.class)){
-
+    if ((!auth.isAuth || auth.loading) && localStorage.getItem("token")) {
+      console.log("test token");
+      testToken();
     }
 
-    if(!shownNotif && auth.isAuth){
-      notification.open({
-        message: 'Please provide beta feedback!',
-        placement: "bottomRight",
-        style: {borderRadius: "10px"},
-        description: (
-          <Text>     
-          Have a feature you want? Found a bug? Please provide any feedback to us <Link href="https://airtable.com/shrJJ9EvbYFOKYXdX" target="_blank">here</Link>
-        </Text>
-        ),
-        duration: "20"
-      });
-
-
-
-
-
-
-      setShownNotif(true)
+    if (
+      auth.isAuth &&
+      !auth.loading &&
+      (!auth.name || !auth.phone || !auth.class)
+    ) {
     }
-
-  }, [auth])
-
+  }, [auth]);
 
   const testToken = async () => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem("token");
 
-    if(!token){
-      console.log("no token")
+    if (!token) {
+      console.log("no token");
     } else {
-      axios.defaults.headers.common['Authorization'] =`Bearer ${token}`
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-      try{
-        console.log("getting user data")
-        const res = await axios.get(`${process.env.REACT_APP_CLUB_API}/user/?timestamp=${new Date().getTime()}`)
-        console.log('respone')
-        console.log(res.data)
-        if(!res.data.errors){
-          setAuth({isAuth: true, user: res.data})
-          console.log("authed")
+      try {
+        console.log("getting user data");
+        const res = await axios.get(
+          `${
+            process.env.REACT_APP_CLUB_API
+          }/user/?timestamp=${new Date().getTime()}`
+        );
+        console.log("respone");
+        console.log(res.data);
+        if (!res.data.errors) {
+          setAuth({ isAuth: true, user: res.data });
+          console.log("authed");
         } else {
-          console.log("failed")
-          localStorage.removeItem("token")
-          setAuth({isAuth: false})
+          console.log("failed");
+          localStorage.removeItem("token");
+          setAuth({ isAuth: false });
         }
       } catch (err) {
-
-        localStorage.removeItem("token")
-        console.log(err.msg)
-        setAuth({isAuth: false})
+        localStorage.removeItem("token");
+        console.log(err.msg);
+        setAuth({ isAuth: false });
       }
     }
-  }
-
-  
+  };
 
   return (
-    <UserClubContext.Provider value={{userClubContext, setUserClubContext}}>
-      <ClubContext.Provider value={{clubContext, setClubContext}}>
-        <AuthContext.Provider value={{auth, setAuth}}>
+    <UserClubContext.Provider value={{ userClubContext, setUserClubContext }}>
+      <ClubContext.Provider value={{ clubContext, setClubContext }}>
+        <AuthContext.Provider value={{ auth, setAuth }}>
           <BrowserRouter>
             <Switch>
-              <Route exact path="/" component={ClubBrowse}/>
-              <Route exact path="/verify/:token" component={Verify}/>
-              <Route exact path="/forgot-password" component={ForgotPassword}/>
-              <Route exact path="/reset-password/:token" component={ResetPassword}/>
-              <Route exact path="/login" component={Login}/>
-              <Route exact path="/signup" component={Signup}/>
-              <Route exact path="/settings" component={Settings}/>
-              <Route exact path ="/clubs/:club" component={Club}/>
-              <Route exact path ="/clubs/:club/settings" component={ClubSettings}/>
+              <Route exact path="/" component={ClubBrowse} />
+              <Route exact path="/verify/:token" component={Verify} />
+              <Route exact path="/forgot-password" component={ForgotPassword} />
+              <Route
+                exact
+                path="/reset-password/:token"
+                component={ResetPassword}
+              />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/settings" component={Settings} />
+              <Route exact path="/clubs/:club" component={Club} />
+              <Route
+                exact
+                path="/clubs/:club/settings"
+                component={ClubSettings}
+              />
               <Route exact path="/team"></Route>
               <Route exact path="/faq"></Route>
               <Route exact path="/contact"></Route>
               <Route exact path="/create" component={ClubCreate}></Route>
             </Switch>
-            <EmailModal testToken={testToken}/>
+            <EmailModal testToken={testToken} />
           </BrowserRouter>
         </AuthContext.Provider>
       </ClubContext.Provider>
     </UserClubContext.Provider>
-   
   );
-}
+};
 
 export default App;
--- src/util/ClubCard.js
+++ src/util/ClubCard.js
@@ -1,51 +1,90 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { UserOutlined, CalendarFilled, CalendarOutlined, CalendarTwoTone, setTwoToneColor} from '@ant-design/icons';
-import { Row, Col, Typography, Button, Avatar, Menu, Card, Select, Tag, Tooltip} from 'antd';
+import {
+  CalendarFilled,
+  CalendarOutlined,
+  CalendarTwoTone,
+  setTwoToneColor,
+  UserOutlined,
+} from "@ant-design/icons";
+import {
+  Avatar,
+  Button,
+  Card,
+  Col,
+  Menu,
+  Row,
+  Select,
+  Tag,
+  Tooltip,
+  Typography,
+} from "antd";
 
-import logo from '../img/icon.jpeg'
+import logo from "../img/icon.jpeg";
 
+const { Title, Text, Paragraph } = Typography;
 
-const {Title , Text, Paragraph} = Typography
-
-
-
-const Navbar = ({clubData}) => {
-  const { name, color, description, sponsors, logo, url, members, officers, tags, tagline, _id} = clubData
+const Navbar = ({ clubData }) => {
+  const {
+    name,
+    color,
+    description,
+    sponsors,
+    logo,
+    url,
+    members,
+    officers,
+    tags,
+    tagline,
+    _id,
+  } = clubData;
   return (
     <Link to={"/clubs/" + url}>
-        <Card key={_id} style={{ width: "31.5rem", borderRadius: "1.5rem", height:"15rem", margin: "0px 2rem 2rem 0px",}} hoverable loading={false}>
-            <Row>
-                    <Col span={17}>
-                        <Title level={4}>{name}</Title>
-                        <div>
-                            {tags.map( tag => <Tag style={{marginBottom:"8px"}}color={color}>{tag}</Tag> )}
-                        </div>
-                    </Col>
-                    <Col span={6} offset={1} align="right">
-                        <Avatar src={logo} style={{borderRadius:"15px"}} size={70}></Avatar>
-                    </Col>
-                </Row>
-                <Paragraph style={{marginTop: "13px", fontSize: "16px", height:"50px"}}>
-                    {tagline}
-                </Paragraph>
-                <Row style={{position: 'relative', bottom: "0px"}}>
-                    <Col span={6} style={{display: "flex", alignItems:"flex-end"}}>
-                        <Text><UserOutlined></UserOutlined> {members.length + officers.length + sponsors.length } </Text>
-                    </Col>
-                    <Col span={12} offset={6} align="right" style={{display: 'none'}}>
-                        <Text style={{letterSpacing:"0.24rem", fontSize: "10px"}}> M T W T F </Text>
-                        <br/>
-                        <Text> Before </Text> <Text style={{marginLeft: "5px"}}> <CalendarOutlined/> <CalendarOutlined/> <CalendarOutlined/> <CalendarOutlined/> <CalendarOutlined/> </Text>
-
-                        <br/>
-                        <Text> After </Text> <Text style={{marginLeft: "5px"}}> <CalendarOutlined/> <CalendarOutlined/> <CalendarOutlined/> <CalendarOutlined/> <Tooltip title="Meeting"> <CalendarFilled style={{color: color}}/>  </Tooltip> </Text>
-                    </Col>
-                </Row>
-        </Card>
+      <Card
+        key={_id}
+        style={{
+          width: "27rem",
+          borderRadius: "1.5rem",
+          height: "15rem",
+          margin: "0px 2rem 2rem 0px",
+        }}
+        hoverable
+        loading={false}
+      >
+        <Row>
+          <Col span={17}>
+            <Title level={4}>{name}</Title>
+            <div>
+              {tags.map((tag) => (
+                <Tag style={{ marginBottom: "8px" }} color={color}>
+                  {tag}
+                </Tag>
+              ))}
+            </div>
+          </Col>
+          <Col span={6} offset={1} align="right">
+            <Avatar
+              src={logo}
+              style={{ borderRadius: "15px" }}
+              size={70}
+            ></Avatar>
+          </Col>
+        </Row>
+        <Paragraph
+          style={{ marginTop: "13px", fontSize: "16px", height: "50px" }}
+        >
+          {tagline}
+        </Paragraph>
+        <div style={{ height: "20rem" }}>
+          <Text style={{ bottom: 0 }}>
+            <UserOutlined></UserOutlined>{" "}
+            {members.length + officers.length + sponsors.length}{" "}
+          </Text>
+        </div>
+      </Card>
     </Link>
   );
-}
+};
 
 export default Navbar;
